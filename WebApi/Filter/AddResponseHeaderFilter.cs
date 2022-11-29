173c4e27f12e145f49bcd4690f94efba315dab28
Tho Nguyen <tho.nguyen@transwap.com>
2022-11-29 09:41:13 +0700

    initialize project

---
+++ WebApi/Filter/AddResponseHeaderFilter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Filter
+{
+	public class AddResponseHeaderFilter : IAsyncActionFilter
+	{
+		public Messages[] Messages { get; set; }
+
+		public AddResponseHeaderFilter(Messages[] messages)
+		{
+			Messages = messages;
+		}
+		public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			foreach (var item in Messages)
+			{
+				context.HttpContext.Response.Headers.Add(item.ToString(), item.ToString());
+			}
+
+			_ = await next.Invoke();
+		}
+	}
+	public class AddResponseHeaderAttribute: TypeFilterAttribute
+	{
+		public AddResponseHeaderAttribute(params Messages[] messages): base(typeof(AddResponseHeaderFilter))
+		{
+			Arguments = new object[] { messages };
+		}
+	}
+
+	public enum Messages
+	{
+		One = 1,
+		Two = 2,
+		Three = 3
+	}
+}
